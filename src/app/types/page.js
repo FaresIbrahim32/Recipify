00764eaf4203550143ce47cdb77ea3d7f8db1d90
fares@LAPTOP-9C455IB9 <faresibrahim@usf.edu>
2023-07-13 21:06:10 -0500

    ssss :wq

---
+++ src/app/types/page.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link'
+
+const fetchRecipes = async()=>{
+    const res = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+
+    const response  = await res.json()
+    return response.meals.map((area) => area.strArea);
+}
+
+
+export default async function page() {
+  
+  const areas = await fetchRecipes();
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-5 server">
+        {areas.map((area, idx) => (
+          <Link
+            key={idx}
+            className="server shadow-gray-50 bg-gray-300 capitalize text-center rounded text-2xl py-10 cursor-pointer font-bold hover:bg-blue-500 hover:text-white"
+            href={`/types/${area}`}
+          >
+            {area}
+          </Link>
+        ))}
+      </div>
+    );
+}
